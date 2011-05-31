8ecce2034a531aa50356630b50f77aa41a39cecd
Nadzuga Vlad <nadzuga0vlad@gmail.com>
2011-06-01 00:17:31 +0300

    Added folder GUI, Added Wait window

--- gui.cpp
+++
@@ -1,7 +0,0 @@
-#include "gui.h"
-
-CGUI::CGUI(QObject *parent) :
-    QObject(parent)
-{
-    connect(m_enterWindow, SIGNAL(SendClientToServer(pIMessage)), this, SIGNAL(SendClientToServer(pIMessage)));
-}
