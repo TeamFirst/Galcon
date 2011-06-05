83e56253479eeec3021b740fd208aba8e094c27a
yuppie <yuppiehoyl@gmail.com>
2011-06-06 00:58:09 +0300

    Fleet fix

--- GUI/Guifleet.cpp
+++ GUI/Guifleet.cpp
@@ -12,27 +12,15 @@ namespace GUI
 
    void CGUIFleet::Draw(QPainter* painter)
    {
-      double d_x(0), d_y(0), fX(0), fY(0);
+      double d_x(0), d_y(0);
       m_fleet->GetPosition(d_x,d_y);
-      m_fleet->GetFixedPoint(fX,fY);
       int x(d_x), y(d_y);
-      int fixX(fX), fixY(fY);
-      //double angle = atan();
-//      painter->setBrush(CGUIPlanet::GetColor(m_fleet->GetPlayerId()));
       painter->setPen(Qt::white);
-//      painter->drawEllipse(x - 5, y - 5, 10 ,10);
-//      painter->drawLine(x,y, fixX, fixY);
-//      painter->setBrush(CGUIPlanet::GetColor(m_fleet->GetPlayerId()));
-//      painter->drawEllipse(fixX - 5, fixY - 5, 10 ,10);
       painter->drawText(x - 15, y - 20,
                QVariant(quint32(m_fleet->GetShipCount())).toString());
       QPixmap image (":/SpaceshipSmall.png");
       QTransform tr;
       tr.rotateRadians(m_fleet->GetAngle());
-
-      //painter->rotate(0.1);
       painter->drawPixmap(x - 22, y - 22, image.transformed(tr));
-
-
    }
 } //Namespace GUI
